51834b82ab24973688f06046fc1d303f137b0df0
Abdiel-88 <abdiel.f@outlook.com>
2024-05-14 13:17:42 -0400

    setup and task 0

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filePath = process.argv[2];
+
+// Read the file content in utf-8
+fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(data);
+});
---
+++ javascript-web_scraping/README.md
@@ -0,0 +1,1 @@
+javascraping readme
