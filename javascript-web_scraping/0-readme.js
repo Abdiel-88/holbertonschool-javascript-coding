28c9f3615934f50fcfbbbc4e53da6bf597f692f2
Abdiel-88 <abdiel.f@outlook.com>
2024-05-14 15:59:27 -0400

    task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,13 +1,18 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+
+if (process.argv.length !== 3) {
+  console.error('Usage: node 0-readme.js <file_path>');
+  process.exit(1);
+}
+
 const filePath = process.argv[2];
 
-// Read the file content in utf-8
 fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
+  if (err) {
+    console.error(err);
+  } else {
     console.log(data);
+  }
 });
