e1057ab9674fd2a8429ffa66a373859755f140ca
Abdiel-88 <abdiel.f@outlook.com>
2024-05-27 16:02:48 -0400

    task 7 fix

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,63 +1,76 @@
 const express = require('express');
 const fs = require('fs');
 
-function countStudents(database) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(database, 'utf8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
-
-      const lines = data.split('\n').filter((line) => line.trim() !== '');
-      if (lines.length <= 1) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
-
-      const students = lines.slice(1).map((line) => line.split(',')).filter((student) => student.length === 4);
-      const numberOfStudents = students.length;
-
-      const fields = {};
-      students.forEach((student) => {
-        const [firstname, , , field] = student;
-        if (!fields[field]) {
-          fields[field] = [];
-        }
-        fields[field].push(firstname);
-      });
-
-      let output = `Number of students: ${numberOfStudents}\n`;
-      for (const [field, names] of Object.entries(fields)) {
-        output += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
-      }
-
-      resolve(output.trim());
-    });
-  });
-}
-
 const app = express();
+const PORT = 1245;
+const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
 
-app.get('/', (req, res) => {
+const countStudents = (dataPath) => new Promise((resolve, reject) => {
+  if (!dataPath) {
+    reject(new Error('Cannot load the database'));
+    return;
+  }
+  fs.readFile(dataPath, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+    const reportParts = [];
+    const fileLines = data.trim().split('\n');
+    const studentGroups = {};
+    const dbFieldNames = fileLines[0].split(',');
+    const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
+
+    for (const line of fileLines.slice(1)) {
+      const studentRecord = line.split(',');
+      const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
+      const field = studentRecord[studentRecord.length - 1];
+      if (!studentGroups[field]) {
+        studentGroups[field] = [];
+      }
+      const studentEntries = studentPropNames.map((propName, idx) => [
+        propName,
+        studentPropValues[idx],
+      ]);
+      studentGroups[field].push(Object.fromEntries(studentEntries));
+    }
+
+    const totalStudents = Object.values(studentGroups).reduce(
+      (pre, cur) => pre + cur.length,
+      0,
+    );
+    reportParts.push(`Number of students: ${totalStudents}`);
+    for (const [field, group] of Object.entries(studentGroups)) {
+      reportParts.push(`Number of students in ${field}: ${group.length}. List: ${group.map((student) => student.firstname).join(', ')}`);
+    }
+    resolve(reportParts.join('\n'));
+  });
+});
+
+app.get('/', (_, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', (req, res) => {
-  const database = process.argv[2];
+app.get('/students', (_, res) => {
+  const responseParts = ['This is the list of our students'];
 
-  countStudents(database)
-    .then((data) => {
-      res.send(`This is the list of our students\n${data}`);
+  countStudents(DB_FILE)
+    .then((report) => {
+      responseParts.push(report);
+      const responseText = responseParts.join('\n');
+      res.setHeader('Content-Type', 'text/plain');
+      res.status(200).send(responseText);
     })
-    .catch((error) => {
-      res.status(500).send(error.message);
+    .catch((err) => {
+      responseParts.push(err.message);
+      const responseText = responseParts.join('\n');
+      res.setHeader('Content-Type', 'text/plain');
+      res.status(500).send(responseText);
     });
 });
 
-const port = 1245;
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
 });
 
 module.exports = app;
